ba166637072bab23db8129503e1520274ea21daa
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-04-04 23:19:33 +0200

    change browser router to Hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import HomePage from "./HomePage";
 import HistoryPage from "./HistoryPage";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 function App() {
   const [transitions, setTransition] = useState([]);
   return (
